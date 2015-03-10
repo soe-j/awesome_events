974f4a8c0a884adb2f868e91d0ad501c0f2d6c14
soe-j <soeda@val.co.jp>
2015-03-10 18:05:33 +0900

    poltergeist, database_cleanerをインストール、設定

--- Gemfile
+++ Gemfile
@@ -56,4 +56,6 @@ end
 group :test do
   gem 'shoulda-matchers'
   gem 'capybara'
+  gem 'poltergeist'
+  gem 'database_cleaner'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,9 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -45,10 +48,29 @@ RSpec.configure do |config|
     })
   end
 
+  config.before(:suite) do
+    DatabaseClaener.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseClaener.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseClaener.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseClaener.start
+  end
+
+  config.after(:each) do
+    DatabaseClaener.clean
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
